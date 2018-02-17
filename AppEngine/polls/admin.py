54247e02c9227565a2a8e708f8c51720c7d844f9
David Thames <davidct@vt.edu>
2018-02-16 22:09:03 -0500

    Changed to django sample

--- AppEngine/polls/admin.py
+++ AppEngine/polls/admin.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
+# Copyright 2015 Google Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,13 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import main
+from django.contrib import admin
 
+from .models import Question
 
-def test_index():
-    main.app.testing = True
-    client = main.app.test_client()
-
-    r = client.get('/')
-    assert r.status_code == 200
-    assert 'Hello World' in r.data.decode('utf-8')
+admin.site.register(Question)
