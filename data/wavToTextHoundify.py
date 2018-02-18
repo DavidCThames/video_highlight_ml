14550622b5e111064224127be70c8576d50286f8
Th-eProgrammer <Dominicabbondanzo@gmail.com>
2018-02-17 17:53:13 -0800

    .

--- data/wavToTextHoundify.py
+++ data/wavToTextHoundify.py
@@ -14,7 +14,7 @@ from multiprocessing.dummy import Pool as ThreadPool
 # returns a dictionary of numbers to strings
 # also adds this dictionary to the list of ones in the database
 # keys of the dictionary will start at 0 and should increment by
-def getTextFromWav(audio_file_name, split_size):
+def getTextFromWav(audio_file_name, split_size, shouldAddToDB):
     myaudio = AudioSegment.from_file(audio_file_name, "wav")
     chunk_length_ms = 1000*split_size  # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms)  # Make chunks of one sec
@@ -41,20 +41,21 @@ def getTextFromWav(audio_file_name, split_size):
     for filename in glob.glob("./temp*"):
         os.remove(filename)
 
-    my_dict = {}
-    try:
-        with open('dataBase.json') as inFile:
-            try:
-                my_dict = json.load(inFile)
-            except ValueError:
-                my_dict = {}
-    except IOError:
+    if shouldAddToDB:
         my_dict = {}
+        try:
+            with open('dataBase.json') as inFile:
+                try:
+                    my_dict = json.load(inFile)
+                except ValueError:
+                    my_dict = {}
+        except IOError:
+            my_dict = {}
 
-    my_dict[audio_file_name] = result
+        my_dict[audio_file_name] = result
 
-    with open("dataBase.json", "w") as jsonFile:
-        json.dump(my_dict, jsonFile)
+        with open("dataBase.json", "w") as jsonFile:
+            json.dump(my_dict, jsonFile)
 
 
     return result
